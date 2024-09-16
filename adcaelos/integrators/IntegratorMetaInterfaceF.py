079a6429fd3b1d4e786912c535a529d989b6ed87
Jonathan Richter <richter.jonathan@outlook.com>
2024-09-15 22:05:55 -0400

    got rid of commented redundant code

--- adcaelos/integrators/IntegratorMetaInterfaceF.py
+++ adcaelos/integrators/IntegratorMetaInterfaceF.py
@@ -1,30 +1,3 @@
-# #IntegratorMetaInterfaceF.py
-
-# import abc
-# import numpy as np
-# from components.ComponentF import Component
-
-
-# class IntegratorMetaInterface(metaclass=abc.ABCMeta):
-#     # @classmethod
-#     # def __subclasshook__(cls, subclass: type) -> bool:
-#     #     return (hasattr(subclass, 'getNextState') and
-#     #             callable(subclass.getNextState) and
-#     #             hasattr(subclass, 'getIntegratorName') and
-#     #             callable(subclass.getIntegratorName) or 
-#     #             NotImplemented)
-    
-#     @abc.abstractmethod
-#     def getNextState(self, fieldObject: Component, currState: np.array, currTime: float, dt: float) -> np.array:
-#         """Returns the next state tied to the next time step
-#         Type: np.array"""
-#         raise NotImplementedError
-        
-#     def getIntegratorName() -> str:
-#         """Returns the name of the integrator class
-#         Type: str """
-#         raise NotImplementedError
-
 import abc
 import numpy as np
 from components.ComponentF import Component
